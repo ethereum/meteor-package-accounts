16b098bd62fe311d3ed765a603977a5996b9ba04
Fabian Vogelsteller <fabian@frozeman.de>
2015-09-03 11:47:23 +0200

    dont expose web3

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'ethereum:accounts',
   summary: 'Provides and updates the ethereum accounts in the Accounts collection',
-  version: '0.2.7',
+  version: '0.2.8',
   git: 'http://github.com/ethereum/meteor-package-accounts'
 });
 
@@ -11,9 +11,9 @@ Package.onUse(function(api) {
   api.use('mongo', ['client', 'server']);
 
   api.use('frozeman:persistent-minimongo@0.1.3', 'client');
-  api.use('ethereum:web3@0.8.1', ['client', 'server']);
+  api.use('ethereum:web3@0.12.2', ['client', 'server']);
 
-  api.export(['EthAccounts', 'web3'], ['client', 'server']); // we need to expose web3.js, so that the app, can re-use this one, instead of having two instances
+  api.export(['EthAccounts'], ['client', 'server']);
 
   api.addFiles('accounts.js', ['client', 'server']);
 });
